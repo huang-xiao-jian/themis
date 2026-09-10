8ecf0bd7b159038ecc27ee1b2489a07bb214463c
黄小健 <hjj491229492@hotmail.com>
2026-09-10 09:28:27 +0800

    chore: upgrade important versions

--- apps/polaris/src/main.tsx
+++ apps/polaris/src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const element = document.getElementById('root')!;
+const root = createRoot(element);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>
